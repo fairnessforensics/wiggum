a4c3e0314da29243e987d134e7e6597eb913a36d
Sarah Brown <sarahmbrown@berkeley.edu>
2017-09-13 18:07:03 -0700

    organization and simple example notebook

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='detect_simpsons_paradox',
+      version='0.1',
+      description='utilities to detect simpson\'s paradox',
+      url='http://github.com/brownsarahm/DetectSimpsonParadox',
+      author='Sarah M Brown',
+      author_email='smb@sarahmbrown.org',
+      license='MIT',
+      packages=['mlhelpers'],
+      zip_safe=False)
