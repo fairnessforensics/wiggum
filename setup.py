0594106ea75826ef0e5f679fcaba80d9889a69b5
Sarah Brown <sarahmbrown@berkeley.edu>
2018-03-09 12:45:14 -0800

    hide requireents for quieter install & upgrade

--- setup.py
+++ setup.py
@@ -8,5 +8,6 @@ setup(name='detect_simpsons_paradox',
       author_email='smb@sarahmbrown.org',
       license='MIT',
       packages=['detect_simpsons_paradox','sp_data_util'],
-      zip_safe=False,
-      install_requires=['matplotlib', 'Numpy', 'Scipy', 'seaborn', 'pandas'])
+      zip_safe=False)
+    #   ,
+    #   install_requires=['matplotlib', 'Numpy', 'Scipy', 'seaborn', 'pandas'])
