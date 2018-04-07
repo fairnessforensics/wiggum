be9e41ca0a1ab1adf4cf8714e1912d26712bcf24
brownsarahm <sarahmbrown@berkeley.edu>
2018-04-07 13:54:44 -0700

    plot cluster representations

--- sp_data_util/spplots.py
+++ sp_data_util/spplots.py
@@ -29,3 +29,67 @@ def sp_plot(df, x_col, y_col, color_col,ci = None,domain_range=[0, 20, 0 , 20]):
     sns.regplot(x_col, y_col, data=df, color='black', scatter=False, ci=ci)
 
     plt.axis(domain_range)
+
+def plot_clustermat(z,fmt=None):
+    """
+
+    black and white matshow for clustering and feat allocation matrices
+
+    Parameters
+    -----------
+    z : nparray, square to be plotted
+    fmt : if z is not a square, then str of what it is
+
+
+    fmt options:
+    'crplist' : a list of values from zero to k
+    'ibplist' : a list of lists of varying lengths
+    'list' : a list, but not nparray otherwise ready to plot
+    """
+
+    K = np.max(z)
+    N = len(z)
+
+    processing = {'crplist': lambda x: list_to_mat(x),
+                  'ibplist': lambda x: make_square(x),
+                  'list': lambda x: np.asarray(x),
+                  None: lambda x: x}
+
+    z_mat = processing[fmt](z)
+
+    # no white grid
+    sns.set_style("whitegrid", {'axes.grid' : False})
+
+    # plot the data
+    plt.matshow(z_mat,cmap=plt.cm.gray_r)
+
+
+    # make the tick marks at the ints
+    ax = plt.gca()
+    ax.set_xticks(np.arange(0, K, 1))
+    ax.set_yticks(np.arange(0, N, 1))
+
+    # Labels for major ticks
+    ax.set_xticklabels(np.arange(0, K, 1))
+    ax.set_yticklabels(np.arange(0, N, 1))
+
+    # Minor ticks at 1/2 marks
+    ax.set_xticks(np.arange(-.5, K, 1), minor=True)
+    ax.set_yticks(np.arange(-.5, N, 1), minor=True)
+
+    # Gridlines based on minor ticks
+    plt.grid(which='minor', color='k', linestyle='-', linewidth=3)
+
+def make_square(z):
+    """
+    convert a list of lists of varying sizes to a square matrix
+    """
+    D = len(z[-1])
+    return np.asarray([np.concatenate((z_i,np.zeros([D-len(z_i)]))) for z_i in z])
+
+def list_to_mat(z):
+    """
+    make a list of length N with values 1 to K into an NxK binanry matrix
+    """
+    tmp = np.eye(K+1)
+    return np.asarray([tmp[z_i] for z_i in z])
