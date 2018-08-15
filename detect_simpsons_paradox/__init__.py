3c9ef7ba8c9a5910c87a040ddaf7cc13716dafa9
brownsarahm <sarahmbrown@berkeley.edu>
2018-08-15 15:39:15 -0400

    working exmaple ranking, still in progress

--- detect_simpsons_paradox/__init__.py
+++ detect_simpsons_paradox/__init__.py
@@ -6,9 +6,11 @@ import itertools as itert
 #from .detect_sp import upper_triangle_df, upper_triangle_element, isReverse,
 from .detect_sp import detect_simpsons_paradox
 from .ranking_processing import (mark_designed_rows, compute_angle,
-compute_slope_all, add_slope_sp, add_angle_col, get_SP_views,
-count_sp_views, add_slope_cols, get_SP_colored_views)
+    compute_slope_all, add_slope_sp, add_angle_col, get_SP_views,
+    count_sp_views, add_slope_cols, get_SP_colored_views,add_view_count,
+    add_weighted)
 
 __all__ = ['detect_simpsons_paradox','mark_designed_rows', 'compute_angle',
             'add_slope_sp','compute_slope_all','add_angle_col','count_sp_views',
-            'add_slope_cols','get_SP_colored_views']
+            'add_slope_cols','get_SP_colored_views','add_view_count',
+            'add_weighted']
