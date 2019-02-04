c48a803374bb77c87f9fe8a2e5975ded48139acf
brownsarahm <sarahmbrown@berkeley.edu>
2019-02-04 17:20:48 -0500

    improved fix

--- setup.py
+++ setup.py
@@ -9,7 +9,6 @@ setup(name='detect_simpsons_paradox',
       license='MIT',
       packages=['detect_simpsons_paradox', 'spviz'],
       zip_safe=False,
-      install_requires=['matplotlib', 'Numpy', 'Scipy', 'seaborn', 'pandas','mlsim','flask'],
-      dependency_links=[
-        'git+git://git@github.com/brownsarahm/ml-sim.git@master#egg=mlsim'
-    ])
+      install_requires=['matplotlib', 'Numpy', 'Scipy', 'seaborn', 'pandas',
+      'mlsim @ https://github.com/brownsarahm/ml-sim/archive/master.zip','flask'],
+      dependency_links=[    ])
