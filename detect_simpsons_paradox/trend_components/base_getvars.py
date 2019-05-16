558ee188488d328f857b4b2767fb25b8a9e0d531
Sarah Brown <sarahmbrown@berkeley.edu>
2019-05-16 16:47:52 -0400

    remove explantory role

--- detect_simpsons_paradox/trend_components/base_getvars.py
+++ detect_simpsons_paradox/trend_components/base_getvars.py
@@ -104,6 +104,6 @@ class weightedMeanRank():
         # maybe not counts
 
         self.target = labeled_df.get_vars_per_roletype('trend',['binary','continuous'])
-        self.trendgroup = labeled_df.get_vars_per_roletype(['trend','explanatory'],'categorical')
+        self.trendgroup = labeled_df.get_vars_per_roletype(['trend','groupby'],'categorical')
         self.var_weight_list = labeled_df.get_weightcol_per_var(self.target)
         return self.target, self.trendgroup
