b6b37f5a83815709e576b6f367ca6b73bf3d955d
Sarah Brown <sarahmbrown@berkeley.edu>
2020-02-26 12:41:54 -0500

    reset index name after append

--- wiggum/data_augmentation.py
+++ wiggum/data_augmentation.py
@@ -37,7 +37,7 @@ class _AugmentedData():
         new_vars = [var for var in data_vars if not(var in meta_vars)]
         # create a new DataFrame withthe right index and columns
         new_vars_df = pd.DataFrame(index = new_vars, columns = META_COLUMNS)
-        new_vars_df.index.name = 'variable'
+        
 
         # set all meta info, all will be the same because they're cluster assignments
         new_vars_df['dtype'] = self.df[new_vars].dtypes
@@ -47,6 +47,7 @@ class _AugmentedData():
 
         # append new rows
         self.meta_df = self.meta_df.append(new_vars_df)
+        self.meta_df.index.name = 'variable'
         return self.meta_df
 
     def add_intersectional(self,var_list=None,tuple_lens=2):
