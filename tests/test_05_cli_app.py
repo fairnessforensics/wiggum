086ad23630957b2e94df4b6ca5a36d5d14e040e8
Sarah Brown <sarahmbrown@berkeley.edu>
2020-03-23 11:19:46 -0400

    file to make cli tests

---
+++ tests/test_05_cli_app.py
@@ -0,0 +1,11 @@
+# class TestConsole(TestCase):
+#     def test_basic(self):
+#         main()
+
+# import pytest
+# import wiggum_app.command_line
+#
+#
+#
+# def test_commandline_app():
+#     wiggum_app.command_line.main()
