11f2d86db257abfc65a148ce93c33f158034cfc1
Sarah Brown <sarahmbrown@berkeley.edu>
2020-03-24 18:58:04 -0400

    correct brackets

--- wiggum/labeled_dataframe.py
+++ wiggum/labeled_dataframe.py
@@ -230,7 +230,7 @@ class LabeledDataFrame(_ResultDataFrame,_TrendDetectors,_AugmentedData):
     def get_trend_by_name(self,trend_name):
         '''
         '''
-        trend_dict = [t.name:i for i,t in enumerate(self.trend_list)]
+        trend_dict = {t.name:i for i,t in enumerate(self.trend_list)}
         return self.trend_list[trend_dict[trend_name]]
 
     def get_trend_display_name(self,trend_res_name):
@@ -242,7 +242,7 @@ class LabeledDataFrame(_ResultDataFrame,_TrendDetectors,_AugmentedData):
         trend_res_name : strings
             name that appears in result_df
         '''
-        trend_dict = [t.name:i for i,t in enumerate(self.trend_list)]
+        trend_dict = {t.name:i for i,t in enumerate(self.trend_list)}
         return self.trend_list[trend_dict[trend_res_name]].display_name
 
     def get_overview_legend_type(self,trend_res_name):
@@ -255,7 +255,7 @@ class LabeledDataFrame(_ResultDataFrame,_TrendDetectors,_AugmentedData):
         trend_res_name : strings
             name that appears in result_df
         '''
-        trend_dict = [t.name:i for i,t in enumerate(self.trend_list)]
+        trend_dict = {t.name:i for i,t in enumerate(self.trend_list)}
         return self.trend_list[trend_dict[trend_res_name]].overview_legend
 
     def get_detail_view_type(self,trend_res_name):
@@ -268,9 +268,9 @@ class LabeledDataFrame(_ResultDataFrame,_TrendDetectors,_AugmentedData):
         trend_res_name : strings
             name that appears in result_df
         '''
-        trend_dict = [t.name:i for i,t in enumerate(self.trend_list)]
+        trend_dict = {t.name:i for i,t in enumerate(self.trend_list)}
         return self.trend_list[trend_dict[trend_res_name]].detail_view
-        detail_view
+
 
 
     def correct_trend_value_datatypes(self):
