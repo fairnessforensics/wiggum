debbe1ebf993b5a23009a182732c25976acea675
Sarah Brown <sarahmbrown@berkeley.edu>
2020-04-16 15:28:27 -0400

    add precoputes, closes #127

--- wiggum/trend_components/classification.py
+++ wiggum/trend_components/classification.py
@@ -107,8 +107,8 @@ class BinClassStats():
                 # compute each stat
                 confusion = df[cur_col].value_counts()
 
-                trend_name = '_'.join([self.name , trend_col_name,g,p])
-                trend_precompute[trend_name] = confusion
+                trend_name = '_'.join([self.name,trend_col_name,groupby_lev,g,p])
+                self.trend_precompute[trend_name] = confusion
 
                 # add values that are not there
                 req_keys = ['TP','TN','FP','FN']
