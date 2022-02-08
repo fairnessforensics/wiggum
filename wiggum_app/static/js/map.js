91563f82cac71a2341c4d8c7e2665390aa9d3365
Shine226 <30415315+Shine226@users.noreply.github.com>
2022-02-08 09:42:04 -0600

    add map test code

--- wiggum_app/static/js/map.js
+++ wiggum_app/static/js/map.js
@@ -5,22 +5,153 @@
  * @returns none.
 */
 function drawMap(data) {
+	var width = 600,
+		height = 500;
+
+	var svg = d3.select('#map')
+				.append('svg')
+				.attr("width", width)
+				.attr("height", height);
+
+	const projection = d3.geoMercator();		
+	//const projection = d3.geoAlbersUsa();				
+	//const projection = d3.geoAlbersUsa().scale(1280)
+    //						.translate([width / 2, height / 2]);
+	const path = d3.geoPath().projection(projection);
+
+
+
+	d3.queue()
+		.defer(d3.json, "../static/map_data/us.json")
+		.defer(d3.json, "../static/map_data/us-congress-113.json")
+		.await(ready);
+
+	function ready(error, us, congress) {
+		if (error) throw error;
+
+		const state_id = 12;
+		const states = topojson.feature(us, us.objects.states);
+		const state = states.features.filter(function(d) { return d.id === state_id; })[0];
+		console.log(us);
+		projection.scale(1)
+				.translate([0, 0]);
+		var b = path.bounds(state),
+		s = 0.95 / Math.max((b[1][0] - b[0][0]) / width, (b[1][1] - b[0][1]) / height),
+		t = [(width - s * (b[1][0] + b[0][0])) / 2, (height - s * (b[1][1] + b[0][1])) / 2];
+	 
+	  	projection.scale(s)
+			.translate(t);
+
+		const districts = topojson.feature(congress, congress.objects.districts);
+		const state_districts = districts.features.filter(function(d) { return checkprefix(d.id, state_id) ; });
+
+		svg.append("g").append("path")
+			.attr("id", "state")
+			.datum(state)
+			.attr("d", path)
+			.attr("fill", "lightgreen");
+
+		svg.append("g")
+			.attr("class", "districts")
+			.selectAll("path")
+			.data(state_districts)
+			.enter().append("path")
+			.attr("d", path)
+			.attr("fill", "yellow")
+			.attr("stroke", "black");			
+	  
+//		svg.append("g")
+//			.attr("class", "districts")
+//		  	.selectAll("path")
+//			  .datum(state_districts)
+//			  .attr("d", path);
+//		  	.data(state_districts)
+//			.data(topojson.feature(congress, congress.objects.districts).features)
+//		  .enter().append("path")
+//			.attr("d", path);
+	  
+//		svg.append("path")
+//			.attr("class", "district-boundaries")
+//			.datum(topojson.mesh(state_districts, state_districts, function(a, b) { return a !== b && (a.id / 1000 | 0) === (b.id / 1000 | 0); }))
+//			.attr("d", path);
+ 
+//		svg.append("path")
+//			.attr("class", "state-boundaries")
+//			.datum(topojson.mesh(us, us.objects.states, function(a, b) { return a !== b; }))
+//			.attr("d", path);
+	  }
+	  
+}
+
+
+/**
+ * Check Prefix
+ * 
+ * @param a - a number.
+ * @param b - a number.* 
+ * @returns true/false.
+*/
+function checkprefix(a, b)
+{     
+    var s1 = a.toString();
+    var s2 = b.toString();
+     
+    var n1 = s1.length;
+    var n2 = s2.length;
+
+    if (n2 != (n1-2))
+    {
+        return false;
+    }
+   
+    for(var i = 0; i < n2; i++)
+    {
+        if (s1[i] != s2[i])
+        {
+            return false;
+        }
+    }
+    return true;
+}
+   
+
+/**
+ * Draw map
+ * 
+ * @param data - result table.
+ * @returns none.
+*/
+function drawMap1(data) {
 	
 	var svg = d3.select('#map')
 				.append('svg')
 				.attr("width", 960)
 				.attr("height", 500);
 
-	const projection = d3.geoMercator();
+	const projection = d3.geoAlbersUsa().scale(1000);
 	const pathGenerator = d3.geoPath().projection(projection);
 
-	d3.json("https://unpkg.com/world-atlas@1.1.4/world/110m.json", function(json_data){
-		const contries = topojson.feature(json_data, json_data.objects.countries);
+	d3.json("../static/map_data/precincts-with-results.geojson", function(json_data){
+		console.log(json_data);
+//		const counties = topojson.feature(json_data, json_data.objects.counties);
 
-		svg.selectAll('path')
-			.data(contries.features)
-			.enter().append('path')
-			.attr('class', 'country')
-			.attr('d', pathGenerator);
+//		const states = topojson.feature(json_data, json_data.objects.states);
+//		const state = states.features.filter(function(d) { return d.id === '40'; });
+
+		svg.selectAll("path")
+//		.data(state)
+		.data(json_data.features)
+		.enter()
+		.append("path")
+		.attr('class', 'country')
+		.attr("d", pathGenerator);
+
+
+//		svg.selectAll('path')
+//			.data(counties.features)
+//			.enter().append('path')
+//			.attr('class', 'country')
+//			.attr('d', pathGenerator);
+
 	});
 }
